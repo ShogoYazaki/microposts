2220fa4f3e5b64e1727b645eaadbf8c048ff97e6
ShogoYazaki <yazaki.s@gmail.com>
2016-05-18 11:15:56 +0000

    0518

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,10 @@ class UsersController < ApplicationController
     if current_user != @user
        redirect_to:root
     end
+    if @user.save
+      flash[:success] = "Up date!"
+      redirect_to @user
+    end
   end
 
 #    @user = User.find(params[:id])
