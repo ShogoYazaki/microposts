ddc9982787699b88edab6ee7520f23aa6c242fef
ShogoYazaki <yazaki.s@gmail.com>
2016-05-19 05:12:21 +0000

    add followings/followers

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   resources :microposts
   resources :users
   resources :sessions, only: [:new, :create, :destroy, :profile, :update]
-
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
